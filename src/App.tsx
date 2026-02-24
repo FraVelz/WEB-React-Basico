23beecc13fd498a4798dd47a0863f76b6e31c50e
Fravelz <fravelz_@hotmail.com>
2026-02-24 15:20:09 -0500

    feat: project basis.

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <main className="min-h-screen bg-slate-950 w-full">
+
+    </main>
+    </>
+  )
+}
+
+export default App
