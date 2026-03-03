7f8baceb978977d0d16b242683c710e565b7a52a
Fravelz <fravelz_@hotmail.com>
2026-03-03 08:45:00 -0500

    update(index.html): add SEO meta tags update(vite.config.ts): add base path

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,4 @@ function App() {
   )
 }
 
-// crear 3 en rayas completo con useEffect y useState, crear un useState para el
-// cursor aparecer y no aparecer con circulo alrrededor
-
 export default App
